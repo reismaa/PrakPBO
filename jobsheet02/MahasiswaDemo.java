bbb4be5a137e42f751153d3bfc93dad6b956e2d7
reismaa <apriliandv07@gmail.com>
2026-09-04 13:57:24 +0700

    Instansiasi 2 objek

--- jobsheet02/MahasiswaDemo.java
+++ jobsheet02/MahasiswaDemo.java
@@ -8,7 +8,24 @@ public class MahasiswaDemo {
         m1.alamat = "Nias, Sumatera Utara";
         m1.kelas = "2A";
 
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "023433";
+        m2.nama = "Haruto Dirgantara";
+        m2.alamat = "Malang, Jawa Timur";
+        m2.kelas = "2C";
+
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "023434";
+        m3.nama = "Elina Bimantara";
+        m3.alamat = "Bandung, Jawa Barat";
+        m3.kelas = "2F";
+
+        System.out.println("Biodata Mahasiswa 1");
         m1.displayBiodata();
+        System.out.println("\nBiodata Mahasiswa 2");
+        m2.displayBiodata();
+        System.out.println("\nBiodata Mahasiswa 3");
+        m3.displayBiodata();  
     }
     
 }
