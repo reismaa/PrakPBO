9019daff92d91c0716782453c628de3c3d0af8aa
reismaa <apriliandv07@gmail.com>
2026-09-11 08:14:56 +0700

    3.1 Percobaan 1 langkah 1 - 7

---
+++ jobsheet03/MotorDemo.java
@@ -0,0 +1,23 @@
+package jobsheet03;
+
+public class MotorDemo {
+    public static void main(String[] args) {
+        Motor motor1 = new Motor();
+        motor1.displayStatus();
+
+        motor1.platNomor = "B 0838 XZ";
+        motor1.kecepatan = 50;
+        motor1.displayStatus();
+
+        Motor motor2 = new Motor();
+        motor2.platNomor = "N 9840 AB";
+        motor2.isMesinOn = true;
+        motor2.kecepatan = 40;
+        motor2.displayStatus();
+
+        Motor motor3 = new Motor();
+        motor3.platNomor = "D 8343 CV";
+        motor3.kecepatan = 60;
+        motor3.displayStatus();
+    }
+}
