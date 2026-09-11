033f06edde0305bcd0fef7ca6755e1d7d4eefd11
reismaa <apriliandv07@gmail.com>
2026-09-11 08:36:38 +0700

    3.1 Percobaan 1 langkah 9 - 10

--- jobsheet03/MotorDemo.java
+++ jobsheet03/MotorDemo.java
@@ -6,18 +6,42 @@ public class MotorDemo {
         motor1.displayStatus();
 
         motor1.platNomor = "B 0838 XZ";
-        motor1.kecepatan = 50;
+        int kecepatanBaru = 50;
+
+        if (!motor1.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } 
+        else {
+            motor1.kecepatan = kecepatanBaru;
+        }
+
         motor1.displayStatus();
 
         Motor motor2 = new Motor();
         motor2.platNomor = "N 9840 AB";
         motor2.isMesinOn = true;
-        motor2.kecepatan = 40;
+        kecepatanBaru = 40;
+
+        if(!motor2.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } 
+        else {
+            motor2.kecepatan = kecepatanBaru;
+        }
+
         motor2.displayStatus();
 
         Motor motor3 = new Motor();
         motor3.platNomor = "D 8343 CV";
-        motor3.kecepatan = 60;
-        motor3.displayStatus();
+        kecepatanBaru = 60;
+
+        if(!motor3.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } 
+        else {
+            motor3.kecepatan = kecepatanBaru;
+        }
+
+        motor3.displayStatus(); 
     }
 }
